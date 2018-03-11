90afb82f75c9a25491574b6014a5e7a7ccfbafd5
Jonas Smith <30472987+JonasESmith@users.noreply.github.com>
2018-03-11 15:33:09 -0500

    Adding core features

--- bot.js
+++ bot.js
@@ -1,16 +1,18 @@
 // Programmers : Jonas & Andrew
 // Date start  : 3/11/2018
 // Purpose     : We are all drunk
-
+//
 var Discord = require('discord.js');
 var winston = require('winston');
 var auth = require('./auth.json');
+
+var list = { };
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    //
+
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
@@ -20,6 +22,7 @@ const logger = winston.createLogger({
 });
 
 // Initialize Discord Bot
+//
 const bot = new Discord.Client();
 
 bot.on("ready", () => {
@@ -28,10 +31,33 @@ bot.on("ready", () => {
   logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+// Checking message for drunk status and !set cmd
+//
 bot.on("message", (message) => {
-  if (message.author.username == "SirArkimedes" && ) {
+  // get current sender of message
+  //
+  var sender = message.author.username;
+
+  // react with 🍺 if drunk = true
+  //
+  if(list[sender] == true && !message.content.startsWith("!set")){
     message.react("🍺")
   }
-});
 
+  // command to add/remove drunk status from dict. & display discord bot message
+  //
+  if(message.content.startsWith("!set") && (sender == "SirArkimedes" || sender == "VOXEL")) {
+      var cmdArray = message.content.split(" ");
+
+      list[cmdArray[1]] = cmdArray[2] == "true" ? true : false;
+
+      // Message is displayed when user is "LIT"
+      //
+      if(list[cmdArray[1]] == true){
+        message.channel.send(cmdArray[1] + " is DRUNK! 🍺🍺🍺🍺");
+      }
+    }
+  });
+
+// Logs in the bot with the auth.token
 bot.login(auth.token);
